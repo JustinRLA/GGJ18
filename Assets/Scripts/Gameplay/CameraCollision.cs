fccfcc09090f5bb7cd2744fd980b751f68f5626d
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2018-01-27 03:38:15 -0500

    PlayerCharacter movement and camera collision management + setup

---
+++ Assets/Scripts/Gameplay/CameraCollision.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour {
+
+    public Transform targetCameraPosition;
+    public Transform player;
+    public float speed;
+    public bool resetCameraPos;
+    public float distanceFromPlayer;
+    public float maxDistanceFromPlayer = 10;
+    public float distanceFromTarget;
+
+    public Quaternion originalRotationValue;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        resetCameraPos = false;
+        originalRotationValue = transform.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        distanceFromPlayer = Vector3.Distance(player.position, transform.position);
+        distanceFromTarget = Vector3.Distance(targetCameraPosition.position, transform.position);
+        
+        if (distanceFromPlayer > maxDistanceFromPlayer)
+        {
+            transform.position = targetCameraPosition.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotationValue, Time.time);
+            //resetCameraPos = false;
+        }
+        if (resetCameraPos)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetCameraPosition.position, step);
+        }
+
+        transform.LookAt(player);
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        resetCameraPos = false;
+        print("collision enter");
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        resetCameraPos = true;
+        print("collision exit");
+    }
+}
---
+++ Assets/Scripts/Gameplay/CharacterControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+    private float leftMove;
+    private float leftRotate;
+    private float rightMove;
+    private float rightRotate;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Movement
+        leftMove = Input.GetAxis("LeftMove") * Time.deltaTime * 5.0f;
+        leftRotate = Input.GetAxis("LeftMove") * Time.deltaTime * 150.0f;
+        rightMove = Input.GetAxis("RightMove") * Time.deltaTime * 5.0f;
+        rightRotate = Input.GetAxis("RightMove") * Time.deltaTime * -150.0f;
+
+        transform.Translate(0, 0, (leftMove + rightMove));
+        transform.Rotate(0, (leftRotate + rightRotate)/5,0);
+    }
+}
