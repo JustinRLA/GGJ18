59c029324b6c857175bfd448c8b9a95e0508f972
AnthonyCousineau <thonycousineau@gmail.com>
2018-01-28 00:17:23 -0500

    Sound and Music
    Intégration des scripts son et musique + création de deux prefable contenant les dit-scripts.

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+
+public class AudioManager : MonoBehaviour {
+
+	public Sound [] sounds;
+
+	public static AudioManager instance;
+
+	// Use this for initialization
+	void Awake () {
+
+		//Destroy the copy of AudioManger if their more than on in a same scene
+		if (instance == null)
+			instance = this;
+		else {
+			Destroy (gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+		foreach (Sound s in sounds) 
+		{
+			s.source = gameObject.AddComponent<AudioSource> ();
+			s.source.clip = s.clip;
+
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.loop;
+		}
+
+	}
+		
+
+	//void Start ()
+	//{
+		//Play ("Theme");
+	//}
+
+	//Search for the sound name and play it
+	public void Play (string name)
+	{
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		if (s == null)
+			//If there a spelling mistake the debug log will tell you witch one is
+			Debug.LogWarning ("Sound: " + name + " not found");
+			return;
+		s.source.Play();
+
+		//To call sound function
+		//FindObjectOfType<AudioManager>().Play("NameOfTheClip");
+	}
+}
---
+++ Assets/Scripts/Audio/RandomFX_Spit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class RandomFX_Spit : MonoBehaviour {
+
+	public AudioClip[] clips;
+	public AudioMixerGroup output;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Need to call this funciton to randomize
+	public void PlayFootstepStone(){
+	
+		// Randomize
+		int randcomClip = Random.Range (0, clips.Length);
+
+		// Create an AudioSource
+		AudioSource source = gameObject.AddComponent<AudioSource>();
+
+		// Load a Clip into the AudioSource
+		source.clip = clips[randcomClip];
+
+		// Set tje output for AudioSource
+		source.outputAudioMixerGroup = output;
+
+		// Play the clip
+		source.Play();
+
+		// Destroy the AudioSource when done playing clip
+		Destroy(source, clips[randcomClip].length);
+	}
+
+
+
+
+
+
+
+}
